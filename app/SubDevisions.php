526993b00f610c31fb50f6f11e0f4124ea9da2e6
Tim Joosten <Tjoosten3@gmail.com>
2017-03-14 14:14:20 +0100

    FIX #251: Create subdivisions table and model.
    Add the model and table. For the subdevisions.
    So We can later embed this data into the country side off this api.
    
    CHANGES:
      * modified:   app/SubDevisions.php
      * new file:   database/migrations/2017_03_14_125433_CreateSubDevisionsTable.php
    
    [DATE]:       Tue Mar 14 14:14:20 2017 +0100
    [SIGNED OFF]: Tim Joosten <topairy@gmail.com>

--- app/SubDevisions.php
+++ app/SubDevisions.php
@@ -4,7 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class SubDevisions
+ * 
+ * @package App
+ */
 class SubDevisions extends Model 
 {
+    /**
+     * The database table name. 
+     * 
+     * @var string
+     */
+    protected $table = 'devisions';
+
+	/**
+	 * Mass-assign fields for the database table. 
+     * 
+	 * @var array
+	 */
 	protected $fillable = ['ISO_3166_2', 'name'];
 }
